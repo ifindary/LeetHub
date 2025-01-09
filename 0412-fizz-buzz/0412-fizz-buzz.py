1c1f4a6153fe917bde355bf8267ea44db0c0db07
Yeon Seonae <154266737+ifindary@users.noreply.github.com>
2025-01-10 00:02:09 +0900

    Time: 0 ms (100%), Space: 18.4 MB (19.02%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -3,13 +3,17 @@ class Solution:
         ans = []
 
         for i in range(1, n+1):
-            if i%15 == 0:
-                ans.append("FizzBuzz")
-            elif i%3 == 0:
-                ans.append("Fizz")
-            elif i%5 == 0:
-                ans.append("Buzz")
-            else:
-                ans.append(str(i))
+            a = i%3 == 0
+            b = i%5 == 0
+            tempStr = ""
+
+            if a:
+                tempStr += "Fizz"
+            if b:
+                tempStr += "Buzz"
+            if tempStr == "":
+                tempStr = str(i)
+            
+            ans.append(tempStr)
         
         return ans
