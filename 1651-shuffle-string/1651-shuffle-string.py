5d76755c690452f4363cb581a7a99c4b71374a75
Yeon Seonae <154266737+ifindary@users.noreply.github.com>
2025-01-21 23:48:23 +0900

    Time: 0 ms (100%), Space: 17.7 MB (50%) - LeetHub

--- 1651-shuffle-string/1651-shuffle-string.py
+++ 1651-shuffle-string/1651-shuffle-string.py
@@ -2,7 +2,7 @@ class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         ans = [-1]*len(s)
 
-        for i, v in enumerate(indices):
-            ans[v] = s[i]
+        for i in range(len(s)):
+            ans[indices[i]] = s[i]
 
         return "".join(ans)
