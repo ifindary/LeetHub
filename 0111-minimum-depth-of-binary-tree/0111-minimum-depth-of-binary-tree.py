2d6796586d1a205d1facbf07c0581bcb96f63936
Yeon Seonae <154266737+ifindary@users.noreply.github.com>
2025-01-26 00:50:13 +0900

    Time: 3 ms (80.6%), Space: 49.9 MB (59.12%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -9,16 +9,20 @@ class Solution:
         # using BFS
         if not root:
             return 0
-
-        queue = deque([(root, 1)])
+        
+        queue = deque([root])
+        depth = 0
 
         while queue:
-            node, depth = queue.popleft()
+            depth += 1
 
-            if not node.left and not node.right:
-                return depth
-            
-            if node.left:
-                queue.append((node.left, depth+1))
-            if node.right:                
-                queue.append((node.right, depth+1))
+            for _ in range(len(queue)):
+                node = queue.popleft()
+
+                if not node.left and not node.right:
+                    return depth
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
