b41ab8bb185372b4b12923045a402bb5884c8807
Yeon Seonae <154266737+ifindary@users.noreply.github.com>
2025-02-05 00:36:07 +0900

    Time: 31 ms (20.35%), Space: 17.8 MB (32.15%) - LeetHub

--- 2752-sum-multiples/2752-sum-multiples.py
+++ 2752-sum-multiples/2752-sum-multiples.py
@@ -1,12 +1,8 @@
 class Solution:
     def sumOfMultiples(self, n: int) -> int:
-        if n < 3:
-            return 0
-        
-        nums = list(range(3, n+1))
         ans = 0
 
-        for n in nums:
+        for n in range(3, n+1):
             if n%3 == 0 or n%5 == 0 or n%7 ==0:
                 ans += n
 
