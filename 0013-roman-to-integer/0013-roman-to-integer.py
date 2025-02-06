abb64ceadf08d4e00a1822b23a6b509caac28382
Yeon Seonae <154266737+ifindary@users.noreply.github.com>
2025-02-07 00:00:11 +0900

    Time: 12 ms (9.45%), Space: 17.9 MB (34.97%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,23 +1,17 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_values = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50,
-        'C': 100, 'D': 500, 'M': 1000
-      }
+        romanValues = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+        }
     
         total = 0
-        prev_value = 0  # 이전 문자의 값 저장
-    
-    # 문자열을 오른쪽에서 왼쪽으로 순회
-        for char in reversed(s):
-            current_value = roman_values[char]
-        
-        # 이전 값보다 작으면 빼기, 그렇지 않으면 더하기
-            if current_value < prev_value:
-                total -= current_value
+
+        for i in range(len(s)-1):
+            if romanValues[s[i]] < romanValues[s[i+1]]:
+                total -= romanValues[s[i]]
             else:
-                total += current_value
-        
-            prev_value = current_value
-    
+                total += romanValues[s[i]]
+
+        total += romanValues[s[-1]]
+
         return total
