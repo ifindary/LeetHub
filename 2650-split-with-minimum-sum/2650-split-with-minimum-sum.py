2dfa202715deac75144f83bfc5372782e7a10d1a
Yeon Seonae <154266737+ifindary@users.noreply.github.com>
2025-02-13 23:27:18 +0900

    Time: 0 ms (100%), Space: 17.6 MB (93.51%) - LeetHub

--- 2650-split-with-minimum-sum/2650-split-with-minimum-sum.py
+++ 2650-split-with-minimum-sum/2650-split-with-minimum-sum.py
@@ -3,14 +3,8 @@ class Solution:
         nums = list(str(num))
         nums.sort()
 
-        nums1 = ""
-        nums2 = ""
-
-        for i in range(0, len(nums), 2):
-            nums1 += str(nums[i])
-
-            if i+1 < len(nums):
-                nums2 += str(nums[i+1])
+        nums1 = ''.join(nums[::2])
+        nums2 = ''.join(nums[1::2])
 
         return int(nums1) + int(nums2)
 
