44da8ef04c012ffe8de00b0623dc58ee4b565b82
Yeon Seonae <154266737+ifindary@users.noreply.github.com>
2025-02-15 00:02:42 +0900

    Time: 8 ms (20.09%), Space: 17.8 MB (82.61%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,10 +5,8 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        num1 = 0
-        num2 = 0
-
         upCnt = 0
+
         node1 = l1
         node2 = l2
 
@@ -16,26 +14,17 @@ class Solution:
         node3 = newList
 
         while node1 or node2 or upCnt:
-            if node1 and node2:
-                tmp = node1.val + node2.val + upCnt
-                node1 = node1.next
-                node2 = node2.next
-            elif node1:
-                tmp = node1.val + upCnt
-                node1 = node1.next
-            elif node2:
-                tmp = node2.val + upCnt
-                node2 = node2.next
-            else:
-                tmp = upCnt
+            val1 = node1.val if node1 else 0
+            val2 = node2.val if node2 else 0
             
-            if tmp >= 10:
-                upCnt = 1
-                tmp -= 10
-            else:
-                upCnt = 0
+            tmp = val1 + val2 + upCnt
+            upCnt = tmp//10
+            tmp %= 10
 
             node3.next = ListNode(tmp)            
             node3 = node3.next
+
+            node1 = node1.next if node1 else None
+            node2 = node2.next if node2 else None
 
         return newList.next
