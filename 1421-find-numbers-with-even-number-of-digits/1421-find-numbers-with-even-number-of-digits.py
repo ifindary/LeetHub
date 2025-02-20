d42dbff63ab40fbdf14d38f643907af04d2a58ea
Yeon Seonae <154266737+ifindary@users.noreply.github.com>
2025-02-20 23:11:03 +0900

    Time: 0 ms (100%), Space: 18 MB (13.32%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.py
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.py
@@ -3,11 +3,7 @@ class Solution:
         ans = 0
 
         for num in nums:
-            tmp1 = num//10
-            tmp2 = num//1000
-            tmp3 = num//100000
-
-            if (tmp1 > 0 and tmp1 < 10) or (tmp2 > 0 and tmp2 < 10) or (tmp3 == 1):
+            if len(str(num))%2 == 0:
                 ans += 1
 
         return ans
