7513f70b62b4744ea3f311b2e100920b263ecced
Yeon Seonae <154266737+ifindary@users.noreply.github.com>
2025-03-25 19:58:07 +0900

    Time: 176 ms (59.03%), Space: 52.9 MB (55.72%) - LeetHub

---
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        meetings.sort()
+        meetingDays = []
+        cnt = 0
+
+        for meeting in meetings:
+            if not meetingDays:
+                meetingDays.append(meeting)
+            elif meetingDays[-1][1] < meeting[0]:
+                meetingDays.append(meeting)
+            else:
+                meetingDays[-1][1] = max(meetingDays[-1][1], meeting[1])
+        
+        for s, e in meetingDays:
+            cnt += (e - s + 1)
+
+        return days - cnt
