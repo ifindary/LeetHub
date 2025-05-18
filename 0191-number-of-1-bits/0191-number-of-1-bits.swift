d44de5327b9fe9db959b175f7ee5ef2f9b67cc89
Yeon Seonae <154266737+ifindary@users.noreply.github.com>
2025-05-18 22:22:44 +0900

    Time: 0 ms (100%), Space: 19.5 MB (53.05%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.swift
+++ 0191-number-of-1-bits/0191-number-of-1-bits.swift
@@ -4,8 +4,8 @@ class Solution {
         var ans = 0
 
         while (n != 0) {
-            ans += n % 2
-            n /= 2 
+            ans += n & 1
+            n >>= 1
         }
 
         return ans
