31283d2805a093f1a97883bd2bc8feb88193e871
Yeon Seonae <154266737+ifindary@users.noreply.github.com>
2025-06-25 00:06:44 +0900

    Time: 0 ms (100%), Space: 19.4 MB (69.23%) - LeetHub

--- 2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.swift
+++ 2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.swift
@@ -1,9 +1,10 @@
 class Solution {
     func isSameAfterReversals(_ num: Int) -> Bool {
-        return num == self.reserveNum(self.reserveNum(num))
+        return num == reserveNum(reserveNum(num))
     }
 
     func reserveNum(_ num: Int) -> Int {
-        return Int(String(String(num).reversed()))!
+        let reserveStr = String(String(num).reversed())
+        return Int(reserveStr)!
     }
 }
