28a821edd37af55d49561c8ca75ad0b20b5a3e3c
Yeon Seonae <154266737+ifindary@users.noreply.github.com>
2025-07-31 23:33:34 +0900

    Time: 4 ms (21.93%), Space: 12.6 MB (0%) - LeetHub

---
+++ 1434-decrypt-string-from-alphabet-to-integer-mapping/1434-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def freqAlphabets(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        ans = []
+
+        i = 0
+
+        while i < len(s):
+            if i+2 < len(s) and s[i+2] == '#':
+                ans.append(chr(int(s[i:i+2])+96))
+                i += 3
+            else:
+                ans.append(chr(int(s[i])+96))
+                i += 1
+        
+        return ''.join(ans)
