def470472d3be9e3a7e4ccb5684e055cc8ce4d9f
Yeon Seonae <154266737+ifindary@users.noreply.github.com>
2025-08-24 23:26:37 +0900

    Time: 3 ms (69.84%), Space: 12.4 MB (90.48%) - LeetHub

--- 3415-check-if-grid-satisfies-conditions/3415-check-if-grid-satisfies-conditions.py
+++ 3415-check-if-grid-satisfies-conditions/3415-check-if-grid-satisfies-conditions.py
@@ -1,27 +1,10 @@
 class Solution(object):
     def satisfiesConditions(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: bool
-        """
-
-        i = 0
-        j = 0
-        ans = True
-
         for i in range(len(grid)):
-            if i+1 < len(grid):
-                print(i, j)
-                ans = ans and grid[i][j] == grid[i+1][j]
-
             for j in range(len(grid[0])):
-                if j+1 < len(grid[0]) and i+1 < len(grid):
-                    ans = ans and grid[i][j+1] == grid[i+1][j+1] and grid[i][j] != grid[i][j+1]
-                elif j+1 < len(grid[0]):
-                    print(i, j)
-                    ans = ans and grid[i][j] != grid[i][j+1]
+                if i+1 < len(grid) and grid[i][j] != grid[i+1][j]:
+                    return False
+                if j+1 < len(grid[0]) and grid[i][j] == grid[i][j+1]:
+                    return False
 
-            if not ans:
-                return ans
-
-        return ans
+        return True
