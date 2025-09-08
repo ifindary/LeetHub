f1e945b8958e2a2fd1dede5e8641911d80a5a419
Yeon Seonae <154266737+ifindary@users.noreply.github.com>
2025-09-08 21:42:48 +0900

    Time: 1 ms (40.18%), Space: 17.7 MB (92.15%) - LeetHub

--- 3568-find-the-key-of-the-numbers/3568-find-the-key-of-the-numbers.py
+++ 3568-find-the-key-of-the-numbers/3568-find-the-key-of-the-numbers.py
@@ -1,12 +1,11 @@
 class Solution:
     def generateKey(self, num1: int, num2: int, num3: int) -> int:
-        str1 = str(num1).rjust(4, '0')
-        str2 = str(num2).rjust(4, '0')
-        str3 = str(num3).rjust(4, '0')
+        nums = [num1, num2, num3]
+        strs = [str(num).rjust(4, '0') for num in nums]
 
         ans = 0
 
         for i in range(4):
-            ans += min(int(str1[i]), int(str2[i]), int(str3[i])) * 10**(3-i)
+            ans += min(int(strs[0][i]), int(strs[1][i]), int(strs[2][i])) * 10**(3-i)
 
         return ans
