2a57a3fa9fba3ebba7e4ad55901891fbb7a062fb
Yeon Seonae <154266737+ifindary@users.noreply.github.com>
2026-01-23 21:39:21 +0900

    Time: 92 ms (55.11%), Space: 31.8 MB (16.44%) - LeetHub

--- 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.py
+++ 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.py
@@ -4,18 +4,17 @@ class Solution:
             return 1
 
         cnt = 1
-        maxNums = nums[0]
-        minNums = min(nums[1:])
+        maxNum = nums[0]
+        minNums = [0]*len(nums)
+        minNums[-1] = nums[-1]
         
-        for i in range(0, len(nums)-1):
-            # print(minNums, nums[i], min(nums[i+1:]))
-            if minNums == nums[i]:
-                minNums = min(nums[i+1:])
-            
-            maxNums = max(maxNums, nums[i])
+        for i in range(len(nums)-2, -1, -1):
+            minNums[i] = min(nums[i], minNums[i+1])
+        
+        for i in range(0, len(nums)-1):            
+            maxNum = max(maxNum, nums[i])
 
-            # print(maxNums, minNums)
-            if maxNums <= minNums:
+            if maxNum <= minNums[i+1]:
                 return cnt
             else:
                 cnt += 1
