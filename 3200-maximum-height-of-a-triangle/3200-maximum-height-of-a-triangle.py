27046837c9a8f4432c9a6b8c63cf6f33b5dae6ed
Yeon Seonae <154266737+ifindary@users.noreply.github.com>
2026-01-27 16:25:14 +0900

    Time: 48 ms (16.08%), Space: 19.3 MB (24.48%) - LeetHub

--- 3200-maximum-height-of-a-triangle/3200-maximum-height-of-a-triangle.py
+++ 3200-maximum-height-of-a-triangle/3200-maximum-height-of-a-triangle.py
@@ -1,36 +1,27 @@
 class Solution:
     def maxHeightOfTriangle(self, red: int, blue: int) -> int:
-        red1 = red2 = red
-        blue1 = blue2 = blue
-
-        curRow1 = curRow2 = 1
+        # 1+3+5+... = k^2
+        # 2+4+6+... = k(k+1)
+        k1 = k2 = 1
 
         # case1: first row is red
-        while red1 >= 0 and blue1 >= 0:
-            if red1 >= curRow1:
-                red1 -= curRow1
-                curRow1 += 1
-            else:
-                break
-            
-            if blue1 >= curRow1:
-                blue1 -= curRow1
-                curRow1 += 1
+        while True:
+            redRow = (k1+1)//2
+            blueRow = k1//2
+            if (red - redRow**2) >= 0 and (blue - blueRow*(blueRow+1)) >= 0:
+                k1 += 1
+                continue
             else:
                 break
 
         # case2: first row is blue
-        while red2 >= 0 and blue2 >= 0:
-            if blue2 >= curRow2:
-                blue2 -= curRow2
-                curRow2 += 1
+        while True:
+            blueRow = (k2+1)//2
+            redRow = k2//2
+            if (blue - blueRow**2) >= 0 and (red - redRow*(redRow+1)) >= 0:
+                k2 += 1
+                continue
             else:
                 break
 
-            if red2 >= curRow2:
-                red2 -= curRow2
-                curRow2 += 1
-            else:
-                break
-            
-        return max(curRow1, curRow2) - 1
+        return max(k1, k2) - 1
