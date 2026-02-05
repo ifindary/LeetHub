ffd2e0436d2e701a007dcdd06e6059b3c49f1789
Yeon Seonae <154266737+ifindary@users.noreply.github.com>
2026-02-05 18:48:36 +0900

    Time: 0 ms (100%), Space: 19.5 MB (24.72%) - LeetHub

---
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumCost(self, nums: List[int]) -> int:
+        minSum = nums[0]
+        nums.remove(minSum)
+        nums.sort()
+
+
+        for i in range(2):
+            minSum += nums[i]
+
+        return minSum
